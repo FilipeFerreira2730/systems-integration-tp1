fb161dedf28e7f4ef995f1efb74fa86cfbeca1df
joaocardoso8 <joao.cardoso@ipvc.pt>
2023-12-08 18:32:54 +0000

    continua filipe, obrigado

--- src/rpc-server/XML/validator.py
+++ src/rpc-server/XML/validator.py
@@ -1,5 +1,5 @@
 import lxml.etree as ET
-def validator(filename):
+"""def validator(filename):
     xmlschema_doc = ET.parse("./rpc-server/XML/premierLeagueXSD.xsd")
     xmlschema = ET.XMLSchema(xmlschema_doc)
 
@@ -10,4 +10,34 @@ def validator(filename):
     for error  in xmlschema.error_log:
         print ("ERROR ON LINE %s: %s" % (error.line, error.message.encode("utf-8")))
 
-    #print(result)
+    #print(result)"""
+
+def validator(filename):
+    try:
+        # Load the XML Schema
+        xmlschema_doc = ET.parse("./rpc-server/XML/ISXSD.xsd")
+        xmlschema = ET.XMLSchema(xmlschema_doc)
+
+        # Load the XML document
+        xml_doc = ET.parse(f"./rpc-server/XML/{filename}.xml")
+
+        # Validate the XML document against the XML Schema
+        result = xmlschema.validate(xml_doc)
+
+        # Print the validation result
+        print(f"Validation Result: {result}")
+
+        # Print any validation errors
+        if not result:
+            print("Validation Errors:")
+            for error in xmlschema.error_log:
+                print(f"ERROR ON LINE {error.line}: {error.message.encode('utf-8')}")
+
+    except ET.XMLSchemaParseError as e:
+        print(f"Error parsing XML Schema: {e}")
+    except ET.XMLSyntaxError as e:
+        print(f"Error parsing XML document: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+validator("test")
