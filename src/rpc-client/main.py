ebfb2739abbbff1a479cd62a939cc82a36e88892
FilipeFerreira2730 <filipemmf10@gmail.com>
2023-12-11 12:14:56 +0000

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/rpc-server/XML/validator.py

--- src/rpc-client/main.py
+++ src/rpc-client/main.py
@@ -13,7 +13,7 @@ while True:
     print("3-Eliminar Ficheiro")
     print("4-Jogos do mundial")
     print("5-Jogos de uma seleção")
-    print("6-Cidade e País com mais jogos") """rever esta querie"""
+    print("6-Cidade e País com mais jogos")
     print("7-Total de golos de uma seleção")
     print("8-Total de golos do Europeu")
     print("9-Média de golos de todos os jogos de seleções")
@@ -40,11 +40,40 @@ while True:
         aux = input()
         server.deleteData(aux)
     elif option == 4:
-        aux=server.string_reverse("O gestor é lindo")
-        print(aux)
+        a = server.jogos_mundial()
+        aux = server.get_connection(a)
+        print("Jogos do mundial:", aux)
+    elif option == 5:
+        print("Selecao: ")
+        selecao = input()
+        a = server.jogos_fora(selecao)
+        aux = server.get_connection(a)
+        print("Jogos da selecao:", aux)
+    elif option == 6:
+        print("Cidade e País com mais jogos:")
+        a = server.cidade_pais_mais_jogos()
+        aux = server.get_connection(a)
+        print("Cidade e País com mais jogos:", aux)
 
-        pass
+    elif option == 7:
+        print("Total de golos de uma seleção:")
+        print("Selecao: ")
+        selecao = input()
+        a = server.total_golos_selecao(selecao)
+        aux = server.get_connection(a)
+        print(f"Total de golos de {selecao}:", aux)
 
+    elif option == 8:
+        print("Total de golos do Europeu:")
+        a = server.total_golos_uefa_euro()
+        aux = server.get_connection(a)
+        print("Total de golos do Europeu:", aux)
+
+    elif option == 9:
+        print("Média de golos de todos os jogos de seleções:")
+        a = server.media_golos_todos_jogos()
+        aux = server.get_connection(a)
+        print("Média de golos de todos os jogos de seleções:", aux)
 
     elif option == 0:
         print("A sair ...")
--- src/rpc-server/main.py
+++ src/rpc-server/main.py
@@ -1,9 +1,14 @@
 import signal, sys
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-from XML import converter
+from XML.converter import converter
+from XML.insertData import insertData
+from XML.validator import validator
+from functions.Connection import get_connection
+from XML.softDeleteExp import deleteData
 from functions.string_length import string_length
 from functions.string_reverse import string_reverse
+from functions.queries import jogos_mundial, jogos_selecao, cidade_pais_mais_jogos, total_golos_selecao, total_golos_uefa_euro, media_golos_todos_jogos
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -31,6 +36,17 @@ with SimpleXMLRPCServer(('0.0.0.0', 9000), requestHandler=RequestHandler) as ser
     server.register_function(string_reverse)
     server.register_function(string_length)
     server.register_function(converter)
+    server.register_function(insertData)
+    server.register_function(validator)
+    server.register_function(get_connection)
+    server.register_function(deleteData)
+    server.register_function(jogos_mundial)
+    server.register_function(jogos_selecao)
+    server.register_function(cidade_pais_mais_jogos)
+    server.register_function(total_golos_selecao)
+    server.register_function(total_golos_uefa_euro)
+    server.register_function(media_golos_todos_jogos)
+    
 
     # start the server
     print("Starting the RPC Server...")
